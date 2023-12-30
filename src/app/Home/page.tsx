d2876f501f7fa6f92e5f3725e4c1cc95bcf78b3a
Sealis04 <suatengco04@gmail.com>
2023-12-30 15:05:28 +0800

    remove packages

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -1,17 +1,10 @@
 
-import {Canvas} from '@react-three/fiber'
 import { Suspense } from 'react'
 export default function Home(){
     return(
         <div>
-            <Canvas 
-                className='w-full h-screen bg-transparent'
-                camera={{near:0.1, far:1000}}
-            >
-                <Suspense fallback={<div></div>}>
-                    
-                </Suspense>
-            </Canvas>
+            <Suspense fallback={<div></div>}>    
+            </Suspense>
         </div>
     )
 }
